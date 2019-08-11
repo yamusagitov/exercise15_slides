aa8874bd4142d461a2e28d702f365070985c80ce
IURII <yamusagitov@gmail.com>
2019-08-11 14:18:46 +0500

    slides

---
+++ js/js.js
@@ -0,0 +1,7 @@
+// 'use strict';
+
+// let age = document.getElementById('age');
+// function showUser(surname, name) {
+// 	alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+// }
+// showUser.apply(age, ["Ivanov", "Alexander"]);
